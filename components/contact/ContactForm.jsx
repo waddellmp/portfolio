13fc95f4282601efb356965001ee10cf7443c65c
Matthew Waddell <waddelmp08@gmail.com>
2023-10-15 23:28:14 -0500

    debug env

--- components/contact/ContactForm.jsx
+++ components/contact/ContactForm.jsx
@@ -4,6 +4,9 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const CommonContact = ({ condition }) => {
+ console.log(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID);
+ console.log(process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID);
+ console.log(process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY);
  const form = useRef();
  // use Email js for recive message
  const sendEmail = (e) => {
