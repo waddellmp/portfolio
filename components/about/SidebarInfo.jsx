677450eb1061ac7112f039edcd61d059846ef0fb
Matthew Waddell <waddelmp08@gmail.com>
2024-01-22 21:46:33 -0600

    Fix broken cv link

--- components/about/SidebarInfo.jsx
+++ components/about/SidebarInfo.jsx
@@ -33,7 +33,7 @@ const SidebarInfo = () => {
     </div>
 
     <a
-     href="/images/cv.pdf"
+     href={withPrefix('/images/cv.pdf')}
      download
      className="inline-flex items-center mx-auto bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-l from-[#DD2476]  to-[#fa5252ef] px-8 py-3 text-lg text-white rounded-[35px] mt-6"
     >
